162e6717a95ba78baaebf40701851b6a68c598ac
Hemanshu Khodiyar <hemanshu.eng@gmail.com>
2019-01-30 14:53:43 +0000

    created Form in vue

---
+++ resources/js/components/Contacts.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+    <h1>Contacts</h1>
+    <form action="#" @submit.prevent="edit ? updateContact(contact.id):createContact()">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input v-model="contact.name" type="text" name ="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input v-model="contact.email" type="text" name ="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="phone">phone</label>
+            <input v-model="contact.phone" type="text" name ="phone" class="form-control">
+        </div>
+        <div class="form-group">
+            <button v-show ="!edit" type="submit" class="btn btn-primary">New Contact</button>
+            <button v-show ="edit" type="submit" class="btn btn-primary">update Contact</button>
+        </div>
+    </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            edit:false,
+            list:[],
+            contact:{
+                id:'',
+                name:'',
+                email:'',
+                phone:''
+            }
+        }
+    },
+    mounted:function(){
+        console.log('contact Componet Loaded');
+    },
+    methods:{
+        createContact:function(){
+            console.log('creating contact');
+            return;
+        },
+        updateContact:function(){
+
+        }
+    }
+}
+</script>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,30 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware'=>'api'],function(){
+        Route::get('contacts',function(){
+            return Contact::latest()->orderBy('created_at','desc')->get();
+        });
+
+    //get single
+        Route::get('contact/{id}',function($id){
+            return Contact::findOrfail($id);
+        });
+
+        Route::post('contact/store', function (Request $request) {
+            return Contact::create(['name'=>$request->input(['name']),'email'=>$request->input(['email']),'phone'=>$request->input(['phone'])]);
+        });
+
+        //update Contact
+
+        Route::patch('contact/{id}',function(Request $request,$id){
+           return  Contact::findOrfail($id)->update(['name'=>$request->input(['name']),'email'=>$request->input(['email']),'phone'=>$request->input(['phone'])]);
+        });
+
+        Route::delete('contact/{id}',function($id){
+            return Contact::destroy($id);
+        });
+    });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
